0bdb70854440afd6243c25e474b510cc1b7e9d3c
ROGELIO LOPEZ BERNAL <22060126@uadeo.mx>
2024-08-23 10:35:45 -0700

    Login Screen Design

--- app/login.tsx
+++ app/login.tsx
@@ -1,16 +1,28 @@
 import { useRouter } from "expo-router";
+import { BlurView } from "expo-blur";
 import { Text, Touchable, TouchableOpacity, View } from "react-native";
-import { TouchableHighlight } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function LoginScreen() {
   const router = useRouter();
   return (
     <SafeAreaView>
-      <View className="justify-center items-center h-full text-black">
-        <Text className="text-black">Hola</Text>
-        <TouchableOpacity onPress={() => router.replace("/(tabs)/")}>
-          <Text>Iniciar Sesion</Text>
+      <View className="justify-center items-center h-full text-black gap-12">
+        <View className="relative">
+          <Text className="text-4xl font-bold text-center text-balance relative z-10">
+            Reunete con tu equipo, organizate y progresa!
+          </Text>
+          <View className="bg-lime-300 h-48 aspect-square rounded-full absolute -top-24 -left-12" />
+          <View className="bg-lime-300 h-20 aspect-square rounded-full absolute -bottom-8 right-2" />
+        </View>
+        <TouchableOpacity onPress={() => router.push("/(tabs)/")}>
+          <View className="bg-lime-400 flex-row items-center p-3 rounded-xl gap-2">
+            <Ionicons color={"white"} name="logo-google" size={24} />
+            <Text className="flex-row rounded-lg text-white">
+              Iniciar Sesion
+            </Text>
+          </View>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
