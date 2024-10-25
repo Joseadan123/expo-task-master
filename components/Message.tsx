c600f3ea32d423c9993da4f815dc16f0bea8dc90
ROGELIO LOPEZ BERNAL <22060126@uadeo.mx>
2024-10-25 08:07:45 -0700

    Message from users

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,10 +1,10 @@
 import { MessageProps } from "@/db/messages";
 import { getUserById, MappedUser } from "@/db/users";
-import { User } from "firebase/auth";
 import { Timestamp } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Image, Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import Avatar from "./Avatar";
+import useUser from "@/hooks/useUser";
 
 export interface IMessage extends Omit<MessageProps, "created_by"> {
   created_by: string;
@@ -12,15 +12,17 @@ export interface IMessage extends Omit<MessageProps, "created_by"> {
 }
 
 export default function Message({ message }: { message: IMessage }) {
-  const [user, setUser] = useState<MappedUser>();
+  const [messageUser, setMessageUser] = useState<MappedUser>();
   const [isUserMessage, setIsUserMessage] = useState(false);
   const [loading, setLoading] = useState(true);
+  const { user } = useUser();
 
   useEffect(() => {
     setLoading(true);
     getUserById(message.created_by)
-      .then((user) => {
-        setUser(user);
+      .then((u) => {
+        setMessageUser(u);
+        console.log(user?.uid, message.created_by);
         setIsUserMessage(user?.uid === message.created_by);
       })
       .finally(() => setLoading(false));
@@ -34,14 +36,16 @@ export default function Message({ message }: { message: IMessage }) {
         isUserMessage ? "flex-row-reverse" : ""
       }`}
     >
-      <Avatar size={42} url={user?.photoUrl as string} />
+      <Avatar size={42} url={messageUser?.photoUrl as string} />
       <View
         className="bg-white w-4/5 rounded-md p-4 gap-1 relative shadow-sm shadow-black/30 z-10"
         style={{
           elevation: Platform.OS == "android" ? 4 : 0,
         }}
       >
-        <Text className="text-lime-700 text-sm">{user?.displayName}</Text>
+        <Text className="text-lime-700 text-sm">
+          {messageUser?.displayName}
+        </Text>
         <Text>{message.content}</Text>
         <Text className="text-sm">
           {new Date(message.created_at.toDate()).toLocaleTimeString()}
